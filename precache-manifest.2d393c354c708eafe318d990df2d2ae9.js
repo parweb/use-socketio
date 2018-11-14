a4f0330da2851526404bd4954cfaf02c5073470b
mfrachet <marvin.frachet@zenika.com>
2018-11-14 14:31:28 +0100

    Updates

---
+++ precache-manifest.2d393c354c708eafe318d990df2d2ae9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba5df8c7943333deb5d2",
+    "url": "/use-socket/static/js/runtime~main.ba5df8c7.js"
+  },
+  {
+    "revision": "65379a5298f2198dafc9",
+    "url": "/use-socket/static/js/main.65379a52.chunk.js"
+  },
+  {
+    "revision": "1a35778b1ec49541bea0",
+    "url": "/use-socket/static/js/1.1a35778b.chunk.js"
+  },
+  {
+    "revision": "65379a5298f2198dafc9",
+    "url": "/use-socket/static/css/main.e2190e63.chunk.css"
+  },
+  {
+    "revision": "6ea38a30fd66c5c37c8ceba14f5487de",
+    "url": "/use-socket/index.html"
+  }
+];
